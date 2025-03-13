3584480f8bfc10fd36cb7ca6b301cce9f1143fd8
Priya Singh <193863733+thinkmetal-priya@users.noreply.github.com>
2025-03-13 13:58:47 +0530

    added right container

---
+++ src/Parallax.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Parallax = () => {
+  return (
+    <div>
+      <section className="container">
+        <div className="news-container ">
+          <div className="one right"></div>
+          <div className="two left"></div>
+          <div className="three right"></div>
+          <div className="four left"></div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Parallax;
--- src/ZoomParallax.jsx
+++ src/ZoomParallax.jsx
@@ -12,17 +12,23 @@ const ZoomParallax = () => {
   console.log(scrollY);
 
   const scale4 = useTransform(scrollYProgress, [0, 1], [1, 6]);
-  const z = useTransform(scrollYProgress, [0, 1], [0, -100]);
-  const tanslateX = useTransform(scrollYProgress, [0, 1], [0, 100]);
+  const scale3 = useTransform(scrollYProgress, [0, 1], [1, 2]);
+  const z = useTransform(scrollYProgress, [0, 1], [-1000, 2000]);
+  const tanslateX = useTransform(scrollYProgress, [0, 0.3], [0, -500]);
   const tanslateMinX = useTransform(scrollYProgress, [0, 1], ["0vw", "100vw"]);
-
+  const z1 = useTransform(scrollYProgress, [0, 1], [-1000, 3000]);
+  const greenTranslateX = useTransform(scrollYProgress, [0, 1], [0, 1500]);
+  const blueTranslateX = useTransform(scrollYProgress, [0, 1], [0, 1000]);
   let miniValue = 500;
 
   return (
     <div ref={container} className="container my-[50vh] h-[200vh]">
-      <div className="container_sticky overflow-hidden flex sticky top-0  perspective-[1000px] bg-amber-400 h-[100vh]">
+      <div
+        className="container_sticky overflow-hidden flex sticky top-0  perspective-[71vw] bg-amber-400 h-[100vh] w-screen 
+      "
+      >
         <motion.div
-          style={{ translateZ: z, translateX: tanslateX }}
+          style={{ translateZ: z, translateX: tanslateX, perspective: "73vw" }}
           className="el w-full h-full absolute top-40 left-60"
         >
           <div className="img_container w-[25vw] h-[25vh] relative">
@@ -34,6 +40,7 @@ const ZoomParallax = () => {
           style={{
             scale: scale4,
             translateX: tanslateX,
+            translateZ: z1,
           }}
           className="el w-full h-full absolute top-40 left-52"
         >
@@ -42,17 +49,28 @@ const ZoomParallax = () => {
           </div>
         </motion.div>
 
-        {/* <motion.div
+        <motion.div
           style={{
-            // scale: scale4,
-            translateX: tanslateMinX,
+            scale: scale3,
+            translateX: blueTranslateX,
           }}
           className="el w-full h-full absolute block top-40 left-[700px]"
         >
           <div className="img_container w-[25vw] h-[25vh] relative">
-            <div className="w-[30vw] rounded-2xl h-[50vh] bg-blue-500"></div>
+            <div className="w-[30vw] rounded-2xl h-[40vh] bg-blue-500 absolute top-6 left-10"></div>
           </div>
-        </motion.div> */}
+        </motion.div>
+        <motion.div
+          style={{
+            scale: scale3,
+            translateX: greenTranslateX,
+          }}
+          className="el w-full h-full absolute block top-40 left-[700px]"
+        >
+          <div className="img_container w-[25vw] h-[25vh] relative">
+            <div className="w-[30vw] rounded-2xl h-[40vh] bg-green-300"></div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
